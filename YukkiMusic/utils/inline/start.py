4de72893695bd434243c315ca3797b262d5834ac
rcrcue <115020933+rcrcue@users.noreply.github.com>
2022-10-25 18:43:30 +0300

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -1,17 +1,10 @@
 #
-
 # Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-
 #
-
 # This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-
 # and is released under the "GNU v3.0 License Agreement".
-
 # Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-
 #
-
 # All rights reserved.
 
 from typing import Union
@@ -19,13 +12,11 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton
 
 from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
-
 from YukkiMusic import app
 
+
 def start_pannel(_):
-
     buttons = [
-
         [
             InlineKeyboardButton(
                 text=_["S_B_1"],
